d7cce7bf451767fc7d63a41059cca3966425b99e
SaumiaSinghal <saumiasinghal.com>
2020-10-21 23:42:50 +0530

    retain dice face on screen rotation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,22 +7,35 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainActivityContract.View {
+    private val DICE_FACE: String = "DICE_FACE"
 
     private val presenter: MainActivityContract.Presenter = MainActivityPresenter(this)
+    private var diceFace: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState?.getInt(DICE_FACE) != null) {
+            diceFace = savedInstanceState.getInt(DICE_FACE, 1)
+        }
+
+        dice_button.setImageResource(presenter.getDiceImageResource(diceFace))
         dice_button.setOnClickListener {presenter.loadResult()}
     }
 
     override fun onSuccess(result: Int) {
-        dice_button.setImageResource(presenter.getDiceImageResource(result))
+        diceFace = result
+        dice_button.setImageResource(presenter.getDiceImageResource(diceFace))
     }
 
     override fun onError(errorMessage: String) {
         toast(errorMessage)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(DICE_FACE, diceFace)
     }
 
     override fun onDestroy() {
@@ -30,6 +43,6 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View {
         presenter.rxUnsubscribe()
     }
 
-    fun Context.toast(message: CharSequence) =
+    private fun Context.toast(message: CharSequence) =
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
